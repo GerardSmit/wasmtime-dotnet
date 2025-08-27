1feaa0095f35f2a9a0a98b6157936a7491220f86
Gerard Smit <gsmit1996@gmail.com>
2025-08-27 22:11:46 +0200

    Use SDK from NuGet

--- GenerateBindings.ps1
+++ GenerateBindings.ps1
@@ -1,25 +1,27 @@
 $WasmtimeGeneratedSource = './src/Interop/Native.cs'
+$WasmtimeSdk = './src/obj/wasmtime-dev-x86_64-windows-c-api/include'
+$WasiPath = Join-Path (Join-Path ( [Environment]::GetFolderPath("UserProfile") ) ".wasi-sdk") "wasi-sdk-25"
 
 ClangSharpPInvokeGenerator `
     -c compatible-codegen exclude-funcs-with-body `
-    --file .\c-api\windows\wasmtime\wasmtime.h `
-    --traverse .\c-api\windows\wasmtime\wasm.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\wasi.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\module.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\instance.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\store.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\error.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\wasip2.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\extern.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\linker.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\sharedmemory.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\component\instance.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\component\val.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\component\linker.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\component\func.h `
-    --traverse .\c-api\windows\wasmtime\wasmtime\component\component.h `
-    --include-directory .\c-api\windows\wasmtime `
-    --include-directory .\c-api\windows\wasi `
+    --file $WasmtimeSdk/wasmtime.h `
+    --traverse $WasmtimeSdk/wasm.h `
+    --traverse $WasmtimeSdk/wasmtime/wasi.h `
+    --traverse $WasmtimeSdk/wasmtime/module.h `
+    --traverse $WasmtimeSdk/wasmtime/instance.h `
+    --traverse $WasmtimeSdk/wasmtime/store.h `
+    --traverse $WasmtimeSdk/wasmtime/error.h `
+    --traverse $WasmtimeSdk/wasmtime/wasip2.h `
+    --traverse $WasmtimeSdk/wasmtime/extern.h `
+    --traverse $WasmtimeSdk/wasmtime/linker.h `
+    --traverse $WasmtimeSdk/wasmtime/sharedmemory.h `
+    --traverse $WasmtimeSdk/wasmtime/component/instance.h `
+    --traverse $WasmtimeSdk/wasmtime/component/val.h `
+    --traverse $WasmtimeSdk/wasmtime/component/linker.h `
+    --traverse $WasmtimeSdk/wasmtime/component/func.h `
+    --traverse $WasmtimeSdk/wasmtime/component/component.h `
+    --include-directory $WasiPath `
+    --include-directory $WasmtimeSdk `
     -n Wasmtime.Interop `
     --methodClassName WasmtimeSource `
     --libraryPath wasmtime `
@@ -30,6 +32,3 @@ ClangSharpPInvokeGenerator `
     -replace "const UIntPtr", "const uint" `
     -replace "sbyte[*]", "byte*" `
     | Out-File $WasmtimeGeneratedSource
-
-    
-#    -replace "(?<name>[a-zA-Z0-9_-]+)\* \@out", 'out ${name} @out' `
